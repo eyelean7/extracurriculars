f4e0f743cd50323c82e2799b2e2ba5653ffaa792
eyelean7 <eyelean7@users.noreply.github.com>
2016-03-18 13:39:18 -0700

    Update extracurriculars.py

--- extracurriculars.py
+++ extracurriculars.py
@@ -1,8 +1,10 @@
-student_activities = {
-    "alice" :    ["math", "art"],
-    "bob" :      ["art", "music"],
-    "charlie" :  ["craft", "math"]
-    }
+student_activities = {}
+#reads csv
+import csv
+with open (r"C:\Users\igorski\Desktop\ext day csv\K-1 Extended Day 2015-2016 Monday.csv") as csvfile:
+     reader = csv.DictReader(csvfile)
+     for row in reader:
+         student_activities.update({row['Name  ']: [row['Notes/Activities ']]})
 
 # adds an activity to 
 def add_roster(activity, roster):
@@ -11,10 +13,6 @@ def add_roster(activity, roster):
             student_activities[name] = [activity]
         else:
             student_activities[name].append(activity)
-        
-
-
-
 
 # deletes an activity from everyone's schedule
 def delete_activity(activity_to_delete):
@@ -22,4 +20,15 @@ def delete_activity(activity_to_delete):
         activities = student_activities[student]
         updated_activities=[activity for activity in activities if activity is not activity_to_delete]
         student_activities[student] = updated_activities
+
+#writes to csv
+def csv_dict_writer():
+    name = "Name  "
+    activities = "Notes/Activities "
+    with open("Finished K-1 Extended Day 2015-2016 Monday.csv", 'w', newline='') as out_file:
+        writer = csv.DictWriter(out_file, (name, activities))
+        writer.writeheader()
+        for student in student_activities:
+            row = {name: student, activities: ' '.join(student_activities[student])}
+            writer.writerow(row)
 
